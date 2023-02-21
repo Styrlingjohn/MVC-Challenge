226fd79142f2fe2c34efa372140f760acc01635e
Styrlingjohn <styrling_morris@yahoo.com>
2023-02-20 17:13:17 -0700

    fixed errors with seeding and misnamed variables

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,10 +1,12 @@
 const router = require('express').Router();
-const { Project, User, Post } = require('../models');
+const { User, Post } = require('../models');
 const withAuth = require('../utils/auth');
 
+
+// Works!
 router.get('/', async (req, res) => {
   try {
-    // Get all projects and JOIN with user data
+    // Get all posts and JOIN with user data
     const postData = await Post.findAll({
       include: [
         {
@@ -22,11 +24,13 @@ router.get('/', async (req, res) => {
       posts, 
       logged_in: req.session.logged_in 
     });
+    
   } catch (err) {
     res.status(500).json(err);
   }
 });
 
+// Works
 router.get('/post/:id', async (req, res) => {
   try {
     const postData = await Post.findByPk(req.params.id, {
@@ -40,17 +44,17 @@ router.get('/post/:id', async (req, res) => {
 
     const post = postData.get({ plain: true });
 
-    res.render('project', {
+    res.render('post', {
       ...post,
-      logged_in: req.session.logged_in
+      // logged_in: req.session.logged_in
     });
   } catch (err) {
     res.status(500).json(err);
   }
 });
 
-// Use withAuth middleware to prevent access to route
-router.get('/profile', withAuth, async (req, res) => {
+// Works!
+router.get('/profile', async (req, res) => {
   try {
     // Find the logged in user based on the session ID
     const userData = await User.findByPk(req.session.user_id, {
@@ -69,6 +73,8 @@ router.get('/profile', withAuth, async (req, res) => {
   }
 });
 
+
+// Works!
 router.get('/login', (req, res) => {
   // If the user is already logged in, redirect the request to another route
   if (req.session.logged_in) {
